f644e3a50392af0ecc63b98811eb295d4850e7db
Getu Gebre <getu.gebre.gg@gmail.com>
2021-03-10 09:04:05 -0600

    commit tree 49fd37f9faf7d5fbead6727b49ef8aa4ecd805e4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
+import { fetchCats } from './actions/catActions';
+import CatList from './CatList'
 class App extends Component {   
-  
+  componentDidMount() {
+    this.props.fetchCats()
+  }
   render() {
+    console.log(this.props.loading)
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        { this.props.loading 
+              ?
+              <h1>Loading....</h1>
+              :
+            <CatList catPics = {this.props.catPics} />
+        }
+        
       </div>
     );
   }
 }
 
-export default App
+const mSTP = state => {
+  return {
+    catPics: state.cats, 
+    loading: state.loading 
+  }
+} 
+const mDTP = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+
+export default connect(mSTP, mDTP)(App)
 
